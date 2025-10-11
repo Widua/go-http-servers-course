c752a8e041b32000643294518b91137dcd979e61
widua <mikolaj.widla@gmail.com>
2025-10-11 19:14:01 +0200

    enhanced handlers

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+	"os"
+)
 
 func main() {
 	serveMux := http.NewServeMux()
@@ -8,7 +11,25 @@ func main() {
 		Handler: serveMux,
 		Addr:    ":8080",
 	}
+	serveMux.HandleFunc("/app", handleApp)
+	serveMux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	serveMux.HandleFunc("/healthz", handleHealthz)
 	serveMux.Handle("/", http.FileServer(http.Dir(".")))
-	serveMux.Handle("/assets/logo.png", http.FileServer(http.Dir(".")))
 	server.ListenAndServe()
+}
+
+func handleApp(responseWriter http.ResponseWriter, req *http.Request) {
+	responseWriter.WriteHeader(200)
+	responseWriter.Header().Add("Content-Type", "application/html")
+	res, err := os.ReadFile("index.html")
+	if err != nil {
+		return
+	}
+	responseWriter.Write(res)
+}
+
+func handleHealthz(responseWriter http.ResponseWriter, req *http.Request) {
+	responseWriter.WriteHeader(200)
+	responseWriter.Header().Add("Content-Type", "text/plain")
+	responseWriter.Write([]byte("OK"))
 }
