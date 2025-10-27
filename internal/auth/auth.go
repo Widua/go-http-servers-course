91d1314a503a8c8a1ecd67a3b58e144418bebbd0
widua <mikolaj.widla@gmail.com>
2025-10-27 20:08:17 +0100

    Implemented JWT creating and validation of tokens, and created basic unit tests

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -1,6 +1,12 @@
 package auth
 
-import "github.com/alexedwards/argon2id"
+import (
+	"fmt"
+	"github.com/alexedwards/argon2id"
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+	"time"
+)
 
 func HashPassword(password string) (string, error) {
 	return argon2id.CreateHash(password, argon2id.DefaultParams)
@@ -9,4 +15,37 @@ func HashPassword(password string) (string, error) {
 func CheckPasswordHash(password string, hash string) (bool, error) {
 	valid, _, err := argon2id.CheckHash(password, hash)
 	return valid, err
+}
+
+func CreateJWTToken(userID uuid.UUID, tokenSecret string, expires time.Duration) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.RegisteredClaims{Issuer: "chirpy", IssuedAt: jwt.NewNumericDate(time.Now()), ExpiresAt: jwt.NewNumericDate(time.Now().Add(expires)), Subject: userID.String()})
+	return token.SignedString(tokenSecret)
+}
+
+func ValidateJWT(tokenString string, tokenSecret string) (uuid.UUID, error) {
+	token, err := jwt.ParseWithClaims(tokenSecret, jwt.RegisteredClaims{}, func(t *jwt.Token) (any, error) {
+		if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", t.Header["alg"])
+		}
+		return tokenSecret, nil
+	})
+	if err != nil {
+		return uuid.UUID{}, err
+	}
+	if issuer, err := token.Claims.GetIssuer(); issuer != "chirpy" {
+		if err != nil {
+			return uuid.UUID{}, err
+		}
+		return uuid.UUID{}, fmt.Errorf("Unexpected issuer: %v", issuer)
+	}
+	subject, err := token.Claims.GetSubject()
+	if err != nil {
+		return uuid.UUID{}, err
+	}
+	userId, err := uuid.Parse(subject)
+	if err != nil {
+		return uuid.UUID{}, err
+	}
+	return userId, nil
+
 }
---
+++ internal/auth/auth_test.go
@@ -0,0 +1,69 @@
+package auth
+
+import (
+	"testing"
+	"time"
+
+	"github.com/alexedwards/argon2id"
+	"github.com/google/uuid"
+)
+
+func hashingTest(t *testing.T) {
+	password := "testPasswd1"
+
+	hash, err := HashPassword(password)
+	if err != nil {
+		t.Errorf("Error occurs while hashing: %v", err)
+	}
+	valid, _, err := argon2id.CheckHash(password, hash)
+	if err != nil {
+		t.Errorf("Error occurs while checking hash: %v", err)
+	}
+	if !valid {
+		t.Errorf("Password, and created hash does not match")
+	}
+}
+
+func HashingCheckTest(t *testing.T) {
+	password := "testPasswd1"
+	hash, _ := HashPassword(password)
+	valid, _, _ := argon2id.CheckHash(password, hash)
+	validAuth, _ := CheckPasswordHash(password, hash)
+
+	if valid != validAuth {
+		t.Errorf("Output from argon2id.CheckHash, and from CheckPasswordHash should match")
+	}
+}
+
+func JWTTokenCycleTest(t *testing.T) {
+	userId := uuid.New()
+	tokenSecret := "secret"
+	expires := time.Second * 5
+
+	token, err := CreateJWTToken(userId, tokenSecret, expires)
+	if err != nil {
+		t.Errorf("Function should create JWT token, but produces error: %v", err)
+	}
+
+	validatedUUID, err := ValidateJWT(token, tokenSecret)
+	if err != nil {
+		t.Errorf("JWT validation should successfully validate JWT produced with CreateJWTToken, but produces error: %v", err)
+	}
+	if validatedUUID != userId {
+		t.Errorf("%v should be equal to %v", userId, validatedUUID)
+	}
+}
+
+func JWTTokenCycleExpiredTest(t *testing.T) {
+	userId := uuid.New()
+	tokenSecret := "secret"
+	expires := time.Millisecond
+
+	token, _ := CreateJWTToken(userId, tokenSecret, expires)
+	time.Sleep(time.Microsecond * 2)
+	validatedUUID, err := ValidateJWT(token, tokenSecret)
+	if err == nil {
+		t.Errorf("JWT Validation should terminate validation when the time from expires parameter pass, and not return with validated UUID, but returned: %v", validatedUUID)
+	}
+
+}
